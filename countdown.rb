94be73c818171a8e495e03397422ef2d22b8e7d1
Ferris Boran <ferris.boran@gmail.com>
2019-03-08 16:49:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while int >= 0
+    case int
+    when int = 1
+      puts "#{int} SECOND!"
+      sleep(1)
+    end
+    puts "#{int} SECONDS!"
+    sleep(1)
+  end
 end
