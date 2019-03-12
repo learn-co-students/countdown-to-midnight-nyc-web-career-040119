7bedcb587853ea636a4de2989b5f9f08e35a3ab1
Edgar Ong <emailedgar@gmail.com>
2019-03-12 21:05:12 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  count = 0
+  while count < int
+    puts "#{int} second(s)!".upcase
+    int -= 1
+  end
+  return "happy new year!".upcase
+end
+
+def countdown_with_sleep(int)
+  sleep 5
+  countdown(int)
 end
