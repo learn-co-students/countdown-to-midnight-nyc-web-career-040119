f90db957bcb176dd6f68808456c1b329d622e605
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-03-19 12:57:04 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+#write your code her
+def countdown(int)
+  int = 10
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    end
+return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(int)
+  int = 10
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    sleep 1
+    int -= 1
+    end
+return "HAPPY NEW YEAR!"
 end
